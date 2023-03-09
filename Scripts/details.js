62c1676d2bd7baa12ff6d936c2bf1f2508b51580
Elcortado <josealejandrobermudez@hotmail.com>
2023-03-09 13:38:17 -0300

    detail

--- Scripts/details.js
+++ Scripts/details.js
@@ -1,25 +1,43 @@
-document.body.onload = agregarCards;
+let eventos = data.events;
+let query = location.search 
+let params = new URLSearchParams(query) 
+let id_query = Number(params.get('_id')) 
 
-function agregarCards() {
-    
-    let card = document.createElement("div");
-    card.setAttribute("class", "row g-0 card-detail");
-    card.innerHTML = `<div class="col-md-4">
-                        <img src="${data.events[5].image}" class="img-fluid rounded-start img-detail" alt="${data.events[0].name}">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">${data.events[5].name}</h5>
-                            <p class="card-text"><small class="text-muted">${data.events[5].date}</small></p>
-                            <p class="card-text">${data.events[5].description}</p>
-                            <p class="card-text">Price: $${data.events[5].price}</p>
-                            <div class="d-flex justify-content-around">
-                                <p class="card-text"><small class="text-muted">Category: ${data.events[5].category}</small></p>
-                                <p class="card-text"><small class="text-muted">Place: ${data.events[5].place}</small></p>
-                                <p class="card-text"><small class="text-muted">Capacity: ${data.events[5].capacity}</small></p>
-                                <p class="card-text"><small class="text-muted">Assistance: ${data.events[5].assistance}</small></p>
-                            </div>
-                        </div>
-                    </div>`;
-    document.getElementById("detail").appendChild(card);
+function defineDetails(detalle) { 
+  let cardQuantity = "";
+  if (detalle.assistance) { 
+    cardQuantity += "Assistance: " + detalle.assistance;
+  }
+  if (detalle.estimate) {
+    cardQuantity += (cardQuantity ? " || " : "") + "Estimate: " + detalle.estimate; 
+  }
+  let cardDetails = 
+      `<div class="col-md-12">
+      <img src="${detalle.image}" class="img-fluid rounded-start img-detail" alt="${detalle.name}">
+  </div>
+  <div class="col-md-12">
+      <div class="card-body">
+          <h5 class="card-title">${detalle.name}</h5>
+          <p class="card-text"><small class="text-muted">${detalle.date}</small></p>
+          <p class="card-text">${detalle.description}</p>
+          <p class="card-text">Price: $${detalle.price}</p>
+          <div class="d-flex justify-content-around">
+              <p class="card-text"><small class="text-muted">Category: ${detalle.category}</small></p>
+              <p class="card-text"><small class="text-muted">Place:  ${detalle.place}</small></p>
+              <p class="card-text"><small class="text-muted">Capacity: ${detalle.capacity}</small></p>
+              <p class="card-text"><small class="text-muted"> ${cardQuantity}</small></p>
+          </div>
+      </div>
+  </div>`;
+  
+  return cardDetails;
 }
+
+function printDetalle(id,det,array_events) {
+  let container = document.querySelector(id); 
+  let dato = array_events.find(each => each._id === det);
+  let details = defineDetails(dato); 
+  container.innerHTML = details; 
+}
+
+printDetalle('#detail', id_query, eventos);
