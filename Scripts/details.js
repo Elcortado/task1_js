64024a0fa67498ca88cd0685339cb32e66db8f25
Elcortado <josealejandrobermudez@hotmail.com>
2023-03-22 12:39:29 -0300

    nuevo

--- Scripts/details.js
+++ Scripts/details.js
@@ -1,26 +1,48 @@
-let $main = document.querySelector("#details");
-let id = new URLSearchParams(document.location.search).get("id");
+const url = 'https://mindhub-xj03.onrender.com/api/amazing'
+const $main = document.getElementById('cardPastEvents')
 
+fetch(url)
+          .then(respuesta => {
+          return respuesta.json()
+          })
+          .then(elemento => {
+          const cartas = elemento.events
+          
+          const params =  new URLSearchParams(location.search)
+          const id = params.get("id")
+          
+          let carta = cartas.find(element => element._id == id)
+          
+          ponerCartas (carta,$main)
 
-fetch('https://mindhub-xj03.onrender.com/api/amazing')
-    .then(respuesta => respuesta.json())
-    .then(datos => renderizarEventoDetallado(datos.events, id, $details))
-    .catch(e => console.log(e));
+          })
 
-function renderizarEventoDetallado(eventos, idEvento, idContenedor) {
-    let eventoObjetivo = eventos.find(evento => evento._id == idEvento);
-    
-    idContenedor.innerHTML += `<div class="container bg-dark p-0 d-flex flex-column gap-2 p-2 w-90 border border-5" id="contenedor-details">
-                                <img src=${eventoObjetivo.image} alt=${eventoObjetivo.name} title=${eventoObjetivo.name} width="100%">
-                                <div>
-                                    <h2 class="text-light text-center fs-1 pb-2"> ${eventoObjetivo.name}</h2>
-                                    <p class="text-light fs-4"> ${eventoObjetivo.description}</p>
-                                    <p class="text-light fs-4">Date: ${eventoObjetivo.date}</p>
-                                    <p class="text-light fs-4">Place: ${eventoObjetivo.place}</p>
-                                    <p class="text-light fs-4">Category: ${eventoObjetivo.category}</p>
-                                    <p class="text-light fs-4">Capacity: ${eventoObjetivo.capacity}</p>
-                                    <p class="text-light fs-4">Estimate: ${eventoObjetivo.assistance}</p>
-                                    <p class="text-light fs-4">Price: ${eventoObjetivo.price}$</p>
-                                </div>
-                            </div>`
+function crearCarta(elemento) {
+    return ` <div class="d-flex justify-content-center align-items-center pt-1">
+    <div class="card mb-3" style="max-width: 640px;">
+      <div class="row g-0 ">
+        <div class="col-md-4">
+          <img src="${elemento.image}" class="img-fluid rounded-start h-100" alt="Imagen de ${elemento.name}">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h6 class="card-title">Name: ${elemento.name}</h6>
+            <h6 class="card-text">Date: ${elemento.date}</h6>
+            <h6 class="card-text">Description:${elemento.description}.</h6>
+            <h6 class="card-text">Category:${elemento.category}</h6>
+            <h6 class="card-text">Place: ${elemento.place}</h6>
+            <h6 class="card-text">Capacity: ${elemento.capacity}</h6>
+            <h6 class="card-text">${elemento.assistance ?'Assistance: '+ elemento.assistance: 'Estimate: '+elemento.estimate}</h6>
+            <h6 class="card-text">Price: $${elemento.price}</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`
 }
+
+function ponerCartas( objeto, element){
+    let template = ''
+    template += crearCarta(objeto)
+    element.innerHTML = template
+} 
